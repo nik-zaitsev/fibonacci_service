d17f34e72e53e9843abcd765e40faa3d027e4421
nik-zaitsev <ve5tiaz@yandex.ru>
2021-10-14 17:30:31 +0300

    fixed http service

--- HTTP_service/HTTP_service.go
+++ HTTP_service/HTTP_service.go
@@ -18,7 +18,7 @@ type JsonAnswer struct {
 	Error          string   `json:"error,omitempty"`
 }
 
-func FillHttpResponseBody(rw http.ResponseWriter, resp *JsonAnswer) {
+func FillHttpResponseBody(rw http.ResponseWriter, resp JsonAnswer) {
 	if jsonResp, err := json.Marshal(resp); err != nil {
 		if _, err := rw.Write([]byte(err.Error())); err != nil {
 			log.Printf("could not write json marshalling error to buffer")
@@ -37,22 +37,22 @@ func (h *HttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	argFrom, errArgFrom := strconv.ParseUint(args.Get("from"), 10, 64)
 	if errArgFrom != nil {
 		log.Printf("fail parsing FROM parameter to uint64: %v", errArgFrom)
-		FillHttpResponseBody(rw, &JsonAnswer{Error: errArgFrom.Error()})
+		FillHttpResponseBody(rw, JsonAnswer{Error: errArgFrom.Error()})
 		return
 	}
 	argTo, errArgTo := strconv.ParseUint(args.Get("to"), 10, 64)
 	if errArgTo != nil {
 		log.Printf("fail parsing TO parameter to uint64: %v", errArgTo)
-		FillHttpResponseBody(rw, &JsonAnswer{Error: errArgTo.Error()})
+		FillHttpResponseBody(rw, JsonAnswer{Error: errArgTo.Error()})
 		return
 	}
 	log.Printf("new request received: From = %d, To = %d", argFrom, argTo)
 	if resSlice, err := fibonacci_calculator.Fibonacci(argFrom, argTo, make(<-chan struct{})); err != nil {
 		log.Printf("bad arguments, %v", err)
-		FillHttpResponseBody(rw, &JsonAnswer{Error: err.Error()})
+		FillHttpResponseBody(rw, JsonAnswer{Error: err.Error()})
 		return
 	} else {
-		FillHttpResponseBody(rw, &JsonAnswer{FibonacciSlice: resSlice})
+		FillHttpResponseBody(rw, JsonAnswer{FibonacciSlice: resSlice})
 		return
 	}
 }
