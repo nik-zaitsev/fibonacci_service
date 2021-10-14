cd91ca7390f2b269f55e9359fa2f53d3f44ebf7f
nik-zaitsev <ve5tiaz@yandex.ru>
2021-10-14 17:28:42 +0300

    added sending error reason to http response

--- HTTP_service/HTTP_service.go
+++ HTTP_service/HTTP_service.go
@@ -13,42 +13,47 @@ import (
 type HttpHandler struct {
 }
 
+type JsonAnswer struct {
+	FibonacciSlice []uint64 `json:"fibonacciSlice,omitempty"`
+	Error          string   `json:"error,omitempty"`
+}
+
+func FillHttpResponseBody(rw http.ResponseWriter, resp *JsonAnswer) {
+	if jsonResp, err := json.Marshal(resp); err != nil {
+		if _, err := rw.Write([]byte(err.Error())); err != nil {
+			log.Printf("could not write json marshalling error to buffer")
+		}
+	} else {
+		if _, err := rw.Write(jsonResp); err != nil {
+			log.Printf("could not write json message to buffer")
+		}
+	}
+	rw.Header().Set("Content-Type", "application/json; charset=utf-8")
+	log.Println("sending back answer...")
+}
+
 func (h *HttpHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	args := r.URL.Query()
 	argFrom, errArgFrom := strconv.ParseUint(args.Get("from"), 10, 64)
 	if errArgFrom != nil {
 		log.Printf("fail parsing FROM parameter to uint64: %v", errArgFrom)
-		rw.WriteHeader(http.StatusBadRequest)
+		FillHttpResponseBody(rw, &JsonAnswer{Error: errArgFrom.Error()})
 		return
 	}
 	argTo, errArgTo := strconv.ParseUint(args.Get("to"), 10, 64)
 	if errArgTo != nil {
 		log.Printf("fail parsing TO parameter to uint64: %v", errArgTo)
-		rw.WriteHeader(http.StatusBadRequest)
+		FillHttpResponseBody(rw, &JsonAnswer{Error: errArgTo.Error()})
 		return
 	}
 	log.Printf("new request received: From = %d, To = %d", argFrom, argTo)
 	if resSlice, err := fibonacci_calculator.Fibonacci(argFrom, argTo, make(<-chan struct{})); err != nil {
 		log.Printf("bad arguments, %v", err)
-		rw.WriteHeader(http.StatusBadRequest)
+		FillHttpResponseBody(rw, &JsonAnswer{Error: err.Error()})
 		return
 	} else {
-		resp := make(map[string][]uint64, 1)
-		resp["fibonacciSlice"] = resSlice
-		if jsonResp, err := json.Marshal(resp); err != nil {
-			if _, err := rw.Write([]byte(err.Error())); err != nil {
-				rw.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-		} else {
-			rw.Header().Set("Content-Type", "application/json; charset=utf-8")
-			rw.WriteHeader(http.StatusOK)
-			if _, err := rw.Write(jsonResp); err != nil {
-				rw.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			log.Printf("sending back answer: %v", resp)
-		}
+		FillHttpResponseBody(rw, &JsonAnswer{FibonacciSlice: resSlice})
+		return
 	}
 }
 
